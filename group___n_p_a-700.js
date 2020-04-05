ba80af91e8375f20deaf611804baec484e91af53
ojousima <ojousima@users.noreply.github.com>
2020-04-05 15:39:26 +0000

    Deploying to gh-pages from  @ e464d2d876dea89c59c7ecaeb55a3606e0d98a83 🚀

--- group___n_p_a-700.js
+++ group___n_p_a-700.js
@@ -31,5 +31,5 @@ var group___n_p_a_700 =
     [ "npa_read_pressure", "group___n_p_a-700.html#gaae6c9c769f5456456288c0abc860c03e", null ],
     [ "npa_read_pressure_temp_hires", "group___n_p_a-700.html#ga3cbcfcba5e5364e38ed17bac3c30afa1", null ],
     [ "npa_read_pressure_temp_lowres", "group___n_p_a-700.html#ga096f850a30db7b17087ed03d6112044d", null ],
-    [ "npa_read_trigger", "group___n_p_a-700.html#gafe47703180044ce9b1ce875e77c36e86", null ]
+    [ "npa_sample_trigger", "group___n_p_a-700.html#ga5e7c2789da43a8632b63f06883d228e3", null ]
 ];
