92d24056c2a8c70bf1381565e65e424689b7041e
Otso Jousimaa <otso@ojousima.net>
2020-04-07 17:43:20 +0300

    Unit test parsing valid values (#3)

--- src/npa_700.c
+++ src/npa_700.c
@@ -1,5 +1,6 @@
 #include "npa_700.h"
 
+#include <stdbool.h>
 #include <stdlib.h>
 
 /**
@@ -46,6 +47,134 @@ static npa_ret_t npa_ctx_check (const npa_ctx_t * const sensor)
     return ret_code;
 }
 
+static npa_ret_t parse_status (const uint8_t data)
+{
+    npa_ret_t ret_code = NPA_SUCCESS;
+    uint32_t status_bits = data >> 6U;
+
+    switch (status_bits)
+    {
+        case 0U:
+            // No action needed
+            break;
+
+        case 1U:
+            // Configuration not supported by this driver, configuration mode is always error.
+            ret_code |= NPA_ERR_MODE;
+            break;
+
+        case 2U:
+            ret_code |= NPA_WARN_OLD;
+            break;
+
+        case 3U:
+        default:
+            ret_code |= NPA_ERR_FATAL;
+            break;
+    }
+
+    return ret_code;
+}
+
+// Return true if value has been saturated.
+static bool check_saturation (const uint16_t value)
+{
+    bool saturated = false;
+
+    if (NPA_PRES_MIN_NONSAT > value)
+    {
+        saturated = true;
+    }
+
+    if (NPA_PRES_MAX_NONSAT < value)
+    {
+        saturated = true;
+    }
+
+    return saturated;
+}
+
+// Get min/max pressure of given sensor
+static npa_ret_t get_scaling (const npa_variant_t model, float * const Pmax,
+                              float * const Pmin)
+{
+    npa_ret_t ret_code = NPA_SUCCESS;
+
+    switch (model)
+    {
+        case NPA_700_02WD:
+            *Pmax = NPA_02WD_SCALE_PA;
+            *Pmin = -1.0F * NPA_02WD_SCALE_PA;
+            break;
+
+        case NPA_700_05WD:
+            *Pmax = NPA_05WD_SCALE_PA;
+            *Pmin = -1.0F * NPA_05WD_SCALE_PA;
+            break;
+
+        case NPA_700_10WD:
+            *Pmax = NPA_10WD_SCALE_PA;
+            *Pmin = -1.0F * NPA_10WD_SCALE_PA;
+            break;
+
+        case NPA_700_001D:
+            *Pmax = NPA_001D_SCALE_PA;
+            *Pmin = -1.0F * NPA_001D_SCALE_PA;
+            break;
+
+        case NPA_700_005D:
+            *Pmax = NPA_005D_SCALE_PA;
+            *Pmin = -1.0F * NPA_005D_SCALE_PA;
+            break;
+
+        case NPA_700_015D:
+            *Pmax = NPA_015D_SCALE_PA;
+            *Pmin = -1.0F * NPA_015D_SCALE_PA;
+            break;
+
+        case NPA_700_030D:
+            *Pmax = NPA_030D_SCALE_PA;
+            *Pmin = -1.0F * NPA_030D_SCALE_PA;
+            break;
+
+        default:
+            ret_code |= NPA_ERR_FATAL;
+            break;
+    }
+
+    return ret_code;
+}
+
+static npa_ret_t parse_value (const npa_variant_t model, const uint8_t * const raw_data,
+                              float * const pressure_pa)
+{
+    npa_ret_t ret_code = NPA_SUCCESS;
+    // Mask status bits out.
+    const uint16_t OUT_U16 = ( ( (uint32_t) raw_data[0U] << 8U)
+                               + (uint32_t) raw_data[1U])
+                             & 0x3FFFU;
+    // Use floats in calculation
+    float Pmin = 0.0F;
+    float Pmax = 0.0F;
+    const float OUTmax = (float) NPA_PRES_MAX_NONSAT;
+    const float OUTmin = (float) NPA_PRES_MIN_NONSAT;
+    const float OUT = (float) OUT_U16;
+
+    if (check_saturation (OUT_U16))
+    {
+        ret_code |= NPA_WARN_SAT;
+    }
+
+    ret_code |= get_scaling (model, &Pmax, &Pmin);
+    /*
+     * Pressure can be calculated from the sensor output using the following formula:
+     *
+     * P = Pmin + (OUT - OUTmin) / (OUTmax - OUTmin) * (Pmax - Pmin)
+     */
+    *pressure_pa = Pmin + (OUT - OUTmin) / (OUTmax - OUTmin) * (Pmax - Pmin);
+    return ret_code;
+}
+
 npa_ret_t npa_sample_trigger (const npa_ctx_t * const sensor)
 {
     npa_ret_t ret_code = npa_ctx_check (sensor);
@@ -65,7 +194,7 @@ npa_ret_t npa_sample_trigger (const npa_ctx_t * const sensor)
 
         - Application guide p.15
         */
-        ret_code |= sensor->read (sensor->npa_addr, NULL, 0);
+        ret_code |= sensor->read (sensor->npa_addr, NULL, 0U);
     }
 
     return ret_code;
@@ -74,7 +203,24 @@ npa_ret_t npa_sample_trigger (const npa_ctx_t * const sensor)
 npa_ret_t npa_read_pressure (const npa_ctx_t * const sensor,
                              float * const pressure_pa)
 {
-    return NPA_ERR_IMPL;
+    npa_ret_t ret_code = npa_ctx_check (sensor);
+
+    if (NULL == pressure_pa)
+    {
+        ret_code |= NPA_ERR_NULL;
+    }
+
+    if (NPA_SUCCESS == ret_code)
+    {
+        // Initialize raw data as all bits set, as it sets internal error code on
+        // by default.
+        uint8_t raw_data[2U] = { 0xFFU, 0xFFU };
+        ret_code |= sensor->read (sensor->npa_addr, raw_data, sizeof (raw_data));
+        ret_code |= parse_status (raw_data[0U]);
+        ret_code |= parse_value (sensor->model, raw_data, pressure_pa);
+    }
+
+    return ret_code;
 }
 
 npa_ret_t npa_read_pressure_temp_lowres (const npa_ctx_t * const sensor,
--- test/test_npa_700.c
+++ test/test_npa_700.c
@@ -4,8 +4,16 @@
 
 #include "mock_i2c.h"
 
-#define NPA_ADDR (0x28U)        //!< Default address of NPA-700
-#define I2C_TX_MAX_LEN (5U)     //!< Maximum length of I2C transfer in bytes
+#include <string.h>
+
+#define NPA_ADDR       (0x28U)  //!< Default address of NPA-700.
+#define I2C_TX_MAX_LEN (5U)     //!< Maximum length of I2C transfer in bytes.
+#define NUM_SENSORS    (7U)     //!< Number of sensor variants.
+#define NUM_VALUES     (3U)     //!< Number of simulated values per sensor.
+
+static const uint8_t min_nonsat_binary[] = { 0x06U, 0x66U};
+static const uint8_t mid_binary[]        = { 0x20U, 0x00U};
+static const uint8_t max_nonsat_binary[] = { 0x39U, 0x99U};
 
 static const npa_ctx_t m_sensor_write_null =
 {
@@ -29,6 +37,153 @@ static const npa_ctx_t m_sensor_valid =
     .read = &i2c_read,
     .npa_addr = NPA_ADDR,
     .model = NPA_700_001D
+};
+static const npa_ctx_t m_sensor_02wd =
+{
+    .write = &i2c_write,
+    .read = &i2c_read,
+    .npa_addr = NPA_ADDR,
+    .model = NPA_700_02WD
+};
+
+#define M_02WD_MIN_NONSAT (-1.0F * NPA_02WD_SCALE_PA)
+#define M_02WD_MIDDLE     (0.0F)
+#define M_02WD_MAX_NONSAT (1.0F * NPA_02WD_SCALE_PA)
+#define M_02WD_VALUES \
+{ \
+  M_02WD_MIN_NONSAT, \
+  M_02WD_MIDDLE, \
+  M_02WD_MAX_NONSAT \
+}
+
+static const npa_ctx_t m_sensor_05wd =
+{
+    .write = &i2c_write,
+    .read = &i2c_read,
+    .npa_addr = NPA_ADDR,
+    .model = NPA_700_05WD
+};
+
+#define M_05WD_MIN_NONSAT (-1.0F * NPA_05WD_SCALE_PA)
+#define M_05WD_MIDDLE     (0.0F)
+#define M_05WD_MAX_NONSAT (1.0F * NPA_05WD_SCALE_PA)
+#define M_05WD_VALUES \
+{ \
+  M_05WD_MIN_NONSAT, \
+  M_05WD_MIDDLE, \
+  M_05WD_MAX_NONSAT \
+}
+
+static const npa_ctx_t m_sensor_10wd =
+{
+    .write = &i2c_write,
+    .read = &i2c_read,
+    .npa_addr = NPA_ADDR,
+    .model = NPA_700_10WD
+};
+
+#define M_10WD_MIN_NONSAT  (-1.0F * NPA_10WD_SCALE_PA)
+#define M_10WD_MIDDLE      (0.0F)
+#define M_10WD_MAX_NONSAT  (1.0F * NPA_10WD_SCALE_PA)
+#define M_10WD_VALUES \
+{ \
+  M_10WD_MIN_NONSAT, \
+  M_10WD_MIDDLE, \
+  M_10WD_MAX_NONSAT \
+}
+
+static const npa_ctx_t m_sensor_001d =
+{
+    .write = &i2c_write,
+    .read = &i2c_read,
+    .npa_addr = NPA_ADDR,
+    .model = NPA_700_001D
+};
+
+#define M_001D_MIN_NONSAT  (-1.0F * NPA_001D_SCALE_PA)
+#define M_001D_MIDDLE      (0.0F)
+#define M_001D_MAX_NONSAT  (1.0F * NPA_001D_SCALE_PA)
+#define M_001D_VALUES \
+{ \
+  M_001D_MIN_NONSAT, \
+  M_001D_MIDDLE, \
+  M_001D_MAX_NONSAT \
+}
+
+static const npa_ctx_t m_sensor_005d =
+{
+    .write = &i2c_write,
+    .read = &i2c_read,
+    .npa_addr = NPA_ADDR,
+    .model = NPA_700_005D
+};
+
+#define M_005D_MIN_NONSAT (-1.0F * NPA_005D_SCALE_PA)
+#define M_005D_MIDDLE     (0.0F)
+#define M_005D_MAX_NONSAT (1.0F * NPA_005D_SCALE_PA)
+#define M_005D_VALUES \
+{ \
+  M_005D_MIN_NONSAT, \
+  M_005D_MIDDLE, \
+  M_005D_MAX_NONSAT \
+}
+
+static const npa_ctx_t m_sensor_015d =
+{
+    .write = &i2c_write,
+    .read = &i2c_read,
+    .npa_addr = NPA_ADDR,
+    .model = NPA_700_015D
+};
+
+#define M_015D_MIN_NONSAT (-1.0F * NPA_015D_SCALE_PA)
+#define M_015D_MIDDLE     (0.0F)
+#define M_015D_MAX_NONSAT (1.0F * NPA_015D_SCALE_PA)
+#define M_015D_VALUES \
+{ \
+  M_015D_MIN_NONSAT, \
+  M_015D_MIDDLE, \
+  M_015D_MAX_NONSAT \
+}
+
+static const npa_ctx_t m_sensor_030d =
+{
+    .write = &i2c_write,
+    .read = &i2c_read,
+    .npa_addr = NPA_ADDR,
+    .model = NPA_700_030D
+};
+
+#define M_030D_MIN_NONSAT (-1.0F * NPA_030D_SCALE_PA)
+#define M_030D_MIDDLE     (0.0F)
+#define M_030D_MAX_NONSAT (1.0F * NPA_030D_SCALE_PA)
+#define M_030D_VALUES \
+{ \
+  M_030D_MIN_NONSAT, \
+  M_030D_MIDDLE, \
+  M_030D_MAX_NONSAT \
+}
+
+static const npa_ctx_t * const m_sensors[7] =
+{
+    &m_sensor_02wd,
+    &m_sensor_05wd,
+    &m_sensor_10wd,
+    &m_sensor_001d,
+    &m_sensor_005d,
+    &m_sensor_015d,
+    &m_sensor_030d
+};
+
+static const float m_expected_values[NUM_SENSORS][NUM_VALUES] =
+{
+    M_02WD_VALUES,
+    M_05WD_VALUES,
+    M_10WD_VALUES,
+    M_001D_VALUES,
+    M_005D_VALUES,
+    M_015D_VALUES,
+    M_030D_VALUES
 };
 
 void setUp (void)
@@ -59,7 +214,7 @@ void test_npa_700_null (void)
  */
 void test_npa_700_sample_trigger (void)
 {
-    i2c_read_ExpectAndReturn (NPA_ADDR, NULL, 0, NPA_SUCCESS);
+    i2c_read_ExpectAndReturn (NPA_ADDR, NULL, 0U, NPA_SUCCESS);
     npa_ret_t ret_code = npa_sample_trigger (&m_sensor_valid);
     TEST_ASSERT (NPA_SUCCESS == ret_code);
 }
@@ -76,9 +231,51 @@ void test_npa_700_sample_trigger (void)
  *
  * @note Use system float as a type.
  */
-npa_ret_t npa_read_pressure (const npa_ctx_t * const sensor,
-                             float * const pressure_pa);
+void test_npa_700_read_pressure (void)
+{
+    float pressure_pa;
+    npa_ret_t ret_code;
 
+    for (size_t sindex = 0U; sindex < NUM_SENSORS; sindex++)
+    {
+        for (size_t vindex = 0U; vindex < NUM_VALUES; vindex++)
+        {
+            uint8_t expect[2] = { 0xFF, 0xFF };
+            uint8_t i2c_result[2];
+
+            if (0U == vindex)
+            {
+                memcpy (i2c_result, min_nonsat_binary, 2U);
+            }
+
+            if (1U == vindex)
+            {
+                memcpy (i2c_result, mid_binary, 2U);
+            }
+
+            if (2U == vindex)
+            {
+                memcpy (i2c_result, max_nonsat_binary, 2U);
+            }
+
+            i2c_read_ExpectWithArrayAndReturn (m_sensors[sindex]->npa_addr, expect, 2U, 2U,
+                                               NPA_SUCCESS);
+            i2c_read_ReturnArrayThruPtr_data (i2c_result, 2U);
+            ret_code = npa_read_pressure (m_sensors[sindex],
+                                          &pressure_pa);
+            TEST_ASSERT (NPA_SUCCESS == ret_code);
+            // Verify 12-bit precision in result.
+            // Divide maximum value by 2^12 to check allowed delta
+            TEST_ASSERT_FLOAT_WITHIN (m_expected_values[sindex][NUM_VALUES - 1U] / 4096.0F,
+                                      m_expected_values[sindex][vindex], pressure_pa);
+        }
+    }
+
+    ret_code = npa_read_pressure (m_sensors[0U], NULL);
+    TEST_ASSERT (NPA_ERR_NULL == ret_code);
+    ret_code = npa_read_pressure (NULL, &pressure_pa);
+    TEST_ASSERT (NPA_ERR_NULL == ret_code);
+}
 /**
  * @brief Read pressure and 8-bit temperature from sensor.
  *
